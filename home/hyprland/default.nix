67cd54f6b372a5dece9cc6f6b0c3ae4bc944e947
wtchrs <wtchr_@hotmail.com>
2025-07-24 23:23:30 +0900

    Add Picture-in-Picture window rules in hyprland

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -55,6 +55,13 @@
         "noanim, selection"
       ];
 
+      windowrulev2 = let
+        pipTitle = "Picture-in-Picture";
+      in [
+        "float,title:${pipTitle}"
+        "pin,title:${pipTitle}"
+      ];
+
       dwindle = {
         pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
         preserve_split = true; # you probably want this
