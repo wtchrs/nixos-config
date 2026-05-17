f18fe3738c3eb79990ce89d5113a4cfb90a9e11e
wtchrs <wtchr_@hotmail.com>
2026-05-17 19:55:22 +0900

    fix(flatpak): update repository URL

--- modules/features/desktop/flatpak.nix
+++ modules/features/desktop/flatpak.nix
@@ -17,7 +17,7 @@
       wantedBy = [ "multi-user.target" ];
       path = [ pkgs.flatpak ];
       script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
       '';
     };
   };
