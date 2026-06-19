a3129d317db99c3bed7c51c3ae62caf4cf5cd804
wtchrs <wtchr_@hotmail.com>
2026-06-20 01:37:49 +0900

    build(nix): remove ComfyUI binary cache

--- modules/core/cache.nix
+++ modules/core/cache.nix
@@ -4,13 +4,10 @@ _: {
       "https://nix-community.cachix.org"
       # CachyOS kernel
       "https://cache.garnix.io"
-      # ComfyUI
-      "https://comfyui.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
     ];
   };
 }
