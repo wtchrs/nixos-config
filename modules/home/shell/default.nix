cbfcb5f5b2cf932388712666bc42a6d03a3980f3
wtchrs <wtchr_@outlook.com>
2026-08-29 23:19:44 +0900

    feat(shell): show zoxide scores in fzf directory picker

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -1,26 +1,5 @@
 { pkgs, ... }:
 
-let
-  fzfLsdPreview = pkgs.writeShellScript "fzf-lsd-preview" ''
-    export _FZF_LSD_PREVIEW_PATH="$1"
-
-    exec ${pkgs.util-linux}/bin/script \
-      --quiet \
-      --return \
-      --command '
-        ${pkgs.coreutils}/bin/stty rows "$FZF_PREVIEW_LINES" cols "$FZF_PREVIEW_COLUMNS";
-        exec ${pkgs.lsd}/bin/lsd --color=always --icon=always --group-directories-first -- "$_FZF_LSD_PREVIEW_PATH"
-      ' \
-      /dev/null
-  '';
-
-  fzfAltCCommand = pkgs.writeShellScript "fzf-alt-c-command" ''
-    {
-      ${pkgs.zoxide}/bin/zoxide query --list
-      ${pkgs.fd}/bin/fd --absolute-path --type=d --hidden --exclude .git
-    } | ${pkgs.gawk}/bin/awk '!seen[$0]++'
-  '';
-in
 {
   imports = [
     ./zsh.nix
@@ -30,6 +9,7 @@ in
     ./zellij
     ./lsd
     ./colored-man.nix
+    ./fzf.nix
   ];
 
   home.packages = with pkgs; [
@@ -43,27 +23,6 @@ in
       config = {
         theme = "Nord";
       };
-    };
-
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-
-      defaultCommand = "${pkgs.fd}/bin/fd --hidden --strip-cwd-prefix --exclude .git";
-      fileWidget.command = "${pkgs.fd}/bin/fd --hidden --strip-cwd-prefix --exclude .git";
-
-      changeDirWidget = {
-        command = "${fzfAltCCommand}";
-        options = [
-          "--no-sort"
-          "--scheme=history"
-          "--preview '${fzfLsdPreview} {}'"
-          "--preview-window=down,30%,sharp"
-        ];
-      };
-
-      # Disable fzf Ctrl+R binding to avoid conflicts with atuin
-      historyWidget.command = "";
     };
 
     broot = {
---
+++ modules/home/shell/fzf.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+let
+  fzfLsdPreview = pkgs.writeShellScript "fzf-lsd-preview" ''
+    export _FZF_LSD_PREVIEW_PATH="$1"
+
+    exec ${pkgs.util-linux}/bin/script \
+      --quiet \
+      --return \
+      --command '
+        ${pkgs.coreutils}/bin/stty rows "$FZF_PREVIEW_LINES" cols "$FZF_PREVIEW_COLUMNS";
+        exec ${pkgs.lsd}/bin/lsd --color=always --icon=always --group-directories-first -- "$_FZF_LSD_PREVIEW_PATH"
+      ' \
+      /dev/null
+  '';
+
+  fzfAltCCommand = pkgs.writeShellScript "fzf-alt-c-command" ''
+    {
+      # zoxide items
+      ${pkgs.zoxide}/bin/zoxide query --list --score |
+        ${pkgs.gawk}/bin/awk '{
+          score = $1
+          sub(/^[[:space:]]*[^[:space:]]+[[:space:]]+/, "")
+          printf "%s\t%s\n", $0, score
+        }'
+
+      # all directories
+      ${pkgs.fd}/bin/fd --absolute-path --type=d --hidden --exclude .git |
+        ${pkgs.gawk}/bin/awk '{ printf "%s\t-\n", $0 }'
+    } | ${pkgs.gawk}/bin/awk -F '\t' '!seen[$1]++'
+  '';
+in
+{
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+
+    defaultCommand = "${pkgs.fd}/bin/fd --hidden --strip-cwd-prefix --exclude .git";
+    fileWidget.command = "${pkgs.fd}/bin/fd --hidden --strip-cwd-prefix --exclude .git";
+
+    changeDirWidget = {
+      command = "${fzfAltCCommand}";
+      options = [
+        "--no-sort"
+        "--scheme=history"
+        "--with-nth='{2}\t{1}'"
+        "--accept-nth=1"
+        "--preview '${fzfLsdPreview} {1}'"
+        "--preview-window=down,30%,sharp"
+      ];
+    };
+
+    # Disable fzf Ctrl+R binding to avoid conflicts with atuin
+    historyWidget.command = "";
+  };
+}
