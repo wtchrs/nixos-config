2038850b330b5bc7170f65e3d0740b676d8c8fe1
wtchrs <wtchr_@outlook.com>
2026-09-17 15:35:35 +0900

    fix(notebook): re-enable systemd-rfkill state persistence

--- configurations/nixos/notebook/system/power.nix
+++ configurations/nixos/notebook/system/power.nix
@@ -1,4 +1,14 @@
+{ lib, ... }:
+
 {
-  services.tlp.enable = true;
-  services.power-profiles-daemon.enable = false;
+  services = {
+    tlp.enable = true;
+    power-profiles-daemon.enable = false;
+  };
+
+  systemd = {
+    # Keep systemd's rfkill state store/restore enabled despite the TLP module masking it
+    services.systemd-rfkill.enable = lib.mkForce true;
+    sockets.systemd-rfkill.enable = lib.mkForce true;
+  };
 }
